4df80307fedd5a347544a68a684fe4335be88d7c
sirknight001 <109674453+sirknight001@users.noreply.github.com>
2022-09-13 00:41:13 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Timer from './components/Timer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
